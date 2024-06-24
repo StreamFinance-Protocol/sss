d1685e80b23f09f56d946ee1c1cbfe6703e4c9e0
soletty <solal@notebooklabs.xyz>
2024-06-23 23:56:30 -0700

    env variables

--- gen_shards.sh
+++ gen_shards.sh
@@ -13,7 +13,19 @@ public_key=$(echo "$output" | sed -n '2p')
 #echo "Ethereum Private Key: $private_key"
 echo "Ethereum Address: $public_key"
 
-export DYDX_ADDRESS=$public_key
+ENV_CONTENTS="HTTPS_PORT=8443
+ETH_RPC=https://eth-mainnet.g.alchemy.com/v2/hh2NUpoXfMpEc2BlQbrR-978GzqVK8Vq
+PORT=3000
+DYDX_ADDRESS=${public_key}
+DYDX_DEPOSIT_ADDRESS=0x8e8bd01b5a9eb272cc3892a2e40e64a716aa2a40
+USDC_ADDRESS=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+DYDX_QUERY_ADDRESS=0x3FeD7bF5Bf3E738bc30fBe61B048fDcb82368545"
+
+# Create the .env file and add the contents
+echo "$ENV_CONTENTS" > "../.env"
+
+# Print a success message
+echo ".env file created with the specified contents."
 
 # Check if private_key is not empty
 if [ -z "$private_key" ]; then
